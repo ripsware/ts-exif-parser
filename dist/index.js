01a3f186f1ccc65fc8b356b7dd0b224a82833ff9
ripsware <rio.prmn@gmail.com>
2020-11-15 00:41:39 +0700

    Add missing index.js.map Add missing index.js

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+export { ExifParserFactory } from './lib/ExifParserFactory';
+export { ExifData } from './lib/ExifData';
+export { OrientationTypes } from './lib/enums';
+export { ThumbnailTypes } from './lib/interfaces';
+//# sourceMappingURL=index.js.map
